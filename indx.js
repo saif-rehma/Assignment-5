1fc0becaac3bd595bed7280dd74122814d06ecff
saif-rehma <144496587+saif-rehma@users.noreply.github.com>
2023-09-23 16:14:41 -0700

    Add files via upload

---
+++ indx.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Quid e Azam";
+let message = famous_person + `once said,
+“Expect the best, prepare for the worst.”`;
+console.log(message);
---
+++ indx.ts
@@ -0,0 +1,5 @@
+
+let famous_person: string = "Quid e Azam";
+let message: string = famous_person + `once said,
+“Expect the best, prepare for the worst.”`
+console.log(message);
